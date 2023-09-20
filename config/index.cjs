f48a9b8a841cb6ba0944eace44f234fbaaebfe2e
jeton_chan <57478051+Jeton-chan@users.noreply.github.com>
2023-09-20 11:15:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oQlX46CZ_R_6LWdMypBBYi28jJcI',
+      id: 'oQlX46C4Yx01VtkbJ7WvnRmgj2_4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'DG89hATMCZl68YhmAvuRwbWHlPB3Zyydj2cLLQa_ebg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
