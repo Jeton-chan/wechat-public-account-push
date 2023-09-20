cbf07ad210027756540d749bc6c456e73766f37e
jeton_chan <57478051+Jeton-chan@users.noreply.github.com>
2023-09-20 11:02:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oQlX46CZ_R_6LWdMypBBYi28jJcI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'fT4-ghn6SYs05JGvC5oGlCuY4AnMa2tkI4tRYImRK-kk',
+      useTemplateId: 'fT4-ghn6SYs05JGvC5oGlCuY4AnMa2tkI4tRYImRK-k',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-13',
       festivals: [
